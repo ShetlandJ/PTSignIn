d579ffad5f73228aebd9b281fcdc602641bcef5e
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-26 22:44:56 +0000

    info passing through routes fine

--- server.js
+++ server.js
@@ -7,11 +7,29 @@ var path = require('path')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/build'));
+// app.use(express.static('client/build/youngperson'));
+
 
 // app.use(require('./controllers/class'));
 
 app.get("/", function(req, res){
-  res.json(path.join(__dirname + '/client/build/index.html'));
+  res.send(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.post("/json", function(req, res){
+
+  console.log(req.body);
+  res.json({
+    'First Time Visit': req.body.visit_switch,
+    'Appointment?': req.body.appt_switch,
+    'Here to see?': req.body.h2s_switch,
+    'Date/Time': Date.now()
+  });
+});
+
+
+app.get("/youngperson", function(req, res){
+  res.sendFile(path.join(__dirname + '/client/build/', 'youngperson.html'));
 });
 
 app.get("/json", function(req, res){
