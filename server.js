9c6f941d425055dda1ba99feccb04b7af5a8794d
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-27 12:23:09 +0000

    production branch

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ MongoClient.connect("mongodb://localhost:27017/signins", function(err, client){
     'Here to see?': 'ME'
   })
 
-  app.listen(3000, function(){
+  app.listen(process.env.PORT || 3000, function(){
     console.log("Listening on port 3000");
   });
 });
