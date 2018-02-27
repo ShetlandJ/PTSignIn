5d9448e1a97840738d46c6b38c295bf63f31fded
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-27 10:00:16 +0000

    fixed front page

--- server.js
+++ server.js
@@ -1,32 +1,70 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var ObjectId = require("mongodb").ObjectId;
 
 var path = require('path')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/build'));
-// app.use(express.static('client/build/youngperson'));
+
+var MongoClient = require("mongodb").MongoClient;
 
 
-// app.use(require('./controllers/class'));
+MongoClient.connect("mongodb://localhost:27017/signins", function(err, client){
+  if (err){
+    return console.log(err);
+  }
+  db = client.db('signins');
+  console.log("connected to db");
+
+  db.collection('signins').insert({
+    'First Visit?': "Heck no",
+    'Appointment?': 'Heck yes',
+    'Here to see?': 'ME'
+  })
+
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+});
+
 
 app.get("/", function(req, res){
   res.send(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.post("/json", function(req, res){
+app.post('/json', function(req, res){
+  //Retrieve data sent by the client in the post request
+  var visiting = req.body.visit_switch;
+  var appt = req.body.appt_switch;
+  var h2s = req.body.h2s_switch;
+  var date = Date.now()
 
-  console.log(req.body);
-  res.json({
-    'First Time Visit': req.body.visit_switch,
-    'Appointment?': req.body.appt_switch,
-    'Here to see?': req.body.h2s_switch,
-    'Date/Time': Date.now()
+  // Insert document in collection
+  db.collection('signins').insert({
+    firstVisit: visiting,
+    appt: appt,
+    hereToSee: h2s,
+    date: date
+  }, function(err, doc) {
+
+    if(err) throw err;
+    console.log("Saved to DB");
+    res.redirect('/');
   });
 });
 
+app.get('/signins', function(req, res) {
+
+  db.collection("signins").find().toArray(function(err, results){
+    if(err){
+      return console.log(err);
+    }
+    res.json(results);
+  });
+});
 
 app.get("/youngperson", function(req, res){
   res.sendFile(path.join(__dirname + '/client/build/', 'youngperson.html'));
@@ -34,9 +72,4 @@ app.get("/youngperson", function(req, res){
 
 app.get("/json", function(req, res){
   res.json({"Heck!": "lo"});
-});
-
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("listening on " + this.address().port );
 });
