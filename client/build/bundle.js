d509c696ebdfdab35644b86b3032ce4cba346f7e
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-02-28 13:18:16 +0000

    chart rendering

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -67,31 +67,14 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-// var app = function() {
-//
-//   const url = 'https://api.mlab.com/api/1/databases/signins/collections/signins?apiKey=Vp2I1nmC961_lV2whDojmmOuZzXb0S_o';
-//
-//   var makeRequest = function(url, callback){
-//     var request = new XMLHttpRequest();
-//     request.open("GET", url);
-//     request.addEventListener("load", callback);
-//     request.send();
-//   }
-//
-//   var requestComplete = function(){
-//     if (this.status !== 200) return;
-//     var jsonString = this.responseText;
-//     var myData = JSON.parse(jsonString);
-//     console.log("Heck")
-//     console.log(myData);
-//   }
-//
-//   makeRequest(url, requestComplete)
-//
-// }
-//
-//
-// window.addEventListener( 'load', app);
+
+var app = function() {
+
+
+}
+
+
+window.addEventListener( 'load', app);
 
 
 /***/ })
